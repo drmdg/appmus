17bb9fc9d6f2d8c8a815156df28899a0579408b9
drmdg <diegobitencort@gmail.com>
2024-07-01 00:44:02 -0300

    fase 1

---
+++ app/functions/twig.php
@@ -0,0 +1,18 @@
+<?php
+
+use app\models\admin\Admin;
+use app\src\Flash;
+
+
+$message = new \Twig_SimpleFunction('message', function ($index) {
+	echo Flash::get($index);
+});
+
+$admin = new \Twig_SimpleFunction('admin', function () {
+	return Admin::getUser();
+});
+
+
+return [
+	$message, $admin,
+];
